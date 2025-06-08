ddefde5c4119144015861b5f8ab5cdd2069acab1
Ana Mirza <anamirza2002@gmail.com>
2025-06-09 00:49:02 +0300

    Modify script to minimize global error

--- src/main.c
+++ src/main.c
@@ -25,21 +25,39 @@ SOFTWARE.
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
+#include <string.h>
+
 #include "StepCountingAlgo.h"
 #include "scoringStage.h"
 #include "detectionStage.h"
 #include "postProcessingStage.h"
-#define PATH "INSERT PATH HERE"
-static int correct_answer = 150;
+#include "motionDetectStage.h"
 
-static void runAlgo(void)
+#define nitems(_a)    (sizeof(_a) / sizeof(0[(_a)]))
+
+struct datapoint
+{
+	char* path;    /* the data of register */
+	int steps;      /* the unit is us */
+};
+
+static const struct datapoint dataset[] = {
+    {"../Dataset/optimize/csv_data/leganat_normal.csv", 150},
+    {"../Dataset/optimize/csv_data/leganat_incet.csv", 150},
+    {"../Dataset/optimize/csv_data/leganat_rapid.csv", 150},
+    {"../Dataset/optimize/csv_data/leganat_alergat.csv", 150},
+    {"../Dataset/optimize/csv_data/buzunar.csv", 170},
+    {"../Dataset/optimize/csv_data/stationar.csv", 150},
+    {"../Dataset/optimize/csv_data/scari.csv", 150},
+};
+
+static void runAlgo(char* path)
 {
     char *line = NULL;
     size_t len = 0;
     size_t read;
-    FILE *fp = fopen(PATH, "r");
+    FILE *fp = fopen(path, "r");
     if (fp == NULL)
     {
         perror("Error while opening the file.\n");
@@ -57,29 +75,54 @@ static void runAlgo(void)
     if (line)
         free(line);
 }
-static int diff = 100000;
+
 int main(int argc, char *argv[])
 {
+    int global_error = 10000;
+
     initAlgo();
-    for (size_t scoringWindowSize = 30; scoringWindowSize <= 40; scoringWindowSize++)
-    {
-        for (int detectionThreshold_whole = 1; detectionThreshold_whole <= 2; detectionThreshold_whole++)
+
+    for (int16_t motionThreshold = 100; motionThreshold <= 300; motionThreshold += 25)
+    {    
+        for (size_t scoringWindowSize = 20; scoringWindowSize <= 30; scoringWindowSize++)
         {
-            for (int detectionThreshold_frac = 1; detectionThreshold_frac <= 9; detectionThreshold_frac++)
+            for (int detectionThreshold_whole = 1; detectionThreshold_whole <= 5; detectionThreshold_whole++)
             {
-                for (int postTimeThreshold = 230; postTimeThreshold <= 280; postTimeThreshold += 5)
+                for (int detectionThreshold_frac = 1; detectionThreshold_frac <= 9; detectionThreshold_frac++)
                 {
-                    changeWindowSize(scoringWindowSize);
-                    changeDetectionThreshold(detectionThreshold_whole, detectionThreshold_frac);
-                    changeTimeThreshold(postTimeThreshold);
-                    runAlgo();
-                    if (diff >= abs(getSteps() - correct_answer))
+                    for (int postTimeThreshold = 100; postTimeThreshold <= 500; postTimeThreshold += 5)
                     {
-                        diff = abs(getSteps() - correct_answer);
-                        printf("Steps=%d windowSize=%zu threshold=%d+1/%d timethreshold=%d diff=%d\n", getSteps(), scoringWindowSize, detectionThreshold_whole, detectionThreshold_frac, postTimeThreshold, abs(correct_answer - getSteps()));
+                        int error = 0;
+                        for (int idx = 0; idx < nitems(dataset); idx++)
+                        {
+                            int correct_answer = dataset[idx].steps;
+
+                            changeWindowSize(scoringWindowSize);
+                            changeDetectionThreshold(detectionThreshold_whole, detectionThreshold_frac);
+                            changeTimeThreshold(postTimeThreshold);
+                            changeMotionThreshold(motionThreshold);
+                            runAlgo(dataset[idx].path);
+
+                            /* Add datapoint error to global error */
+                            error += abs(getSteps() - correct_answer);
+
+                            resetSteps();
+                            resetAlgo();
+                        }
+
+                        /* Find best parameter combination across datapoints */
+                        if (error <= global_error) {
+                            global_error = error;
+                            printf("Error=%d windowSize=%zu threshold=%d+1/%d timethreshold=%d motionthreshold=%d\n", 
+                                global_error, 
+                                scoringWindowSize, 
+                                detectionThreshold_whole, 
+                                detectionThreshold_frac, 
+                                postTimeThreshold,
+                                motionThreshold);
+                            fflush(stdout);
+                        }
                     }
-                    resetSteps();
-                    resetAlgo();
                 }
             }
         }
